827c2760b5c2a02cd0e3f3b7280aac3f89ba9517
Patryk Cisek <patryk@debian.org>
2011-03-03 14:48:01 +0100

    Added stub CWMPEvent

---
+++ src/cwmpd/cwmpEvent.cpp
@@ -0,0 +1,5 @@
+#include "cwmpEvent.h"
+
+CWMPEvent::CWMPEvent()
+{
+}
---
+++ src/cwmpd/cwmpEvent.h
@@ -0,0 +1,10 @@
+#ifndef CWMPEVENT_H
+#define CWMPEVENT_H
+
+class CWMPEvent
+{
+public:
+    CWMPEvent();
+};
+
+#endif // CWMPEVENT_H
