4f47655b1f8db352dd73021b77d92e81fa1052c7
Eugene Bogush <eugeneb2008@gmail.com>
2019-03-30 18:18:41 +0200

    clean

--- libbson-1.0-config.cmake
+++
@@ -1,5 +0,0 @@
-# Specify the minimum version you require.
- include_directories("/usr/include/libmongoc-1.0")
- include_directories("/usr/include/libbson-1.0")
- target_link_libraries(main.o /usr/lib/x86_64-linux-gnu/libmongoc-1.0.so.0 /usr/lib/x86_64-linux-gnu/libbson-1.0.so.0)
-
